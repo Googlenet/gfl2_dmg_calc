aaced307df10976ac3d1e17187d419ee524de8f6
Jonah Hu <jonah.hu123@gmail.com>
2026-04-07 15:10:37 -0700

    updatd foods with relevant dmg tags

--- js/data/food_buffs.js
+++ js/data/food_buffs.js
@@ -27,14 +27,14 @@ const FOOD_BUFFS = [
     id:          'big_mex',
     name:        'Big Mex',
     description: 'When making Out-Of-Turn attacks, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { ootAtkPct: 2.5 },
   },
 
   {
     id:          'black_truffle',
     name:        'Black Truffle Double Beefburger',
     description: 'Damage dealt by active attacks which are not Ultimate skills is increased by 3%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 3 },
+    buff:        { activeDmgPct: 3 },
   },
 
   {
@@ -48,7 +48,7 @@ const FOOD_BUFFS = [
     id:          'borscht',
     name:        'Borscht',
     description: 'When dealing Physical damage, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { physAtkPct: 2.5 },
   },
 
   {
@@ -69,28 +69,28 @@ const FOOD_BUFFS = [
     id:          'buttered_escargots',
     name:        'Buttered Escargots',
     description: 'Damage dealt by Ultimate skills is increased by 3%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 3 },
+    buff:        { ultDmgPct: 3 },
   },
 
   {
     id:          'country_style',
     name:        'Country-style Stir Fry',
     description: 'When dealing Electric damage, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { elecAtkPct: 2.5 },
   },
 
   {
     id:          'doner_kebab',
     name:        'Doner Kebab',
     description: 'When dealing Burn damage, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { burnAtkPct: 2.5 },
   },
 
   {
     id:          'faraway_hulatang',
     name:        'Faraway Hulatang',
     description: 'Hydro damage dealt is increased by 2%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 2 },
+    buff:        { hydroDmgPct: 2 },
   },
 
   {
@@ -104,21 +104,21 @@ const FOOD_BUFFS = [
     id:          'hot_sour_pork',
     name:        'Hot and Sour Double-Cooked Pork',
     description: 'Targeted damage dealt is increased by 3%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 3 },
+    buff:        { targetDmgPct: 3 },
   },
 
   {
     id:          'kofta',
     name:        'Kofta',
     description: 'When dealing Hydro damage, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { hydroAtkPct: 2.5 },
   },
 
   {
     id:          'miso_oden',
     name:        'Miso Oden',
     description: 'Damage dealt by Confectance skills is increased by 3%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 3 },
+    buff:        { ciDmgPct: 3 },
   },
 
   {
@@ -132,70 +132,70 @@ const FOOD_BUFFS = [
     id:          'prawn_platter',
     name:        'Prawn Platter',
     description: 'Freeze damage dealt is increased by 2%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 2 },
+    buff:        { freezeDmgPct: 2 },
   },
 
   {
     id:          'prawn_potstickers',
     name:        'Prawn Potstickers',
     description: 'When making an active attack, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { activeAtkPct: 2.5 },
   },
 
   {
     id:          'roasted_shawarma',
     name:        'Roasted Shawarma',
     description: 'Burn damage dealt is increased by 2%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 2 },
+    buff:        { burnDmgPct: 2 },
   },
 
   {
     id:          'rosemary_chicken',
     name:        'Rosemary Chicken',
     description: 'Damage dealt by non-Confectance skills is increased by 3%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 3 },
+    buff:        { nonciDmgPct: 3 },
   },
 
   {
     id:          'sanxian_jiaozi',
     name:        'Sanxian Jiaozi',
     description: 'Physical damage dealt is increased by 2%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 2 },
+    buff:        { physDmgPct: 2 },
   },
 
   {
     id:          'sea_snails',
     name:        'Sea Snails in Spicy Wine',
     description: 'When dealing Freeze damage, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { freezeAtkPct: 2.5 },
   },
 
   {
     id:          'seafood_sashimi',
     name:        'Seafood Sashimi',
     description: 'AoE damage is increased by 3%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 3 },
+    buff:        { aoeDmgPct: 3 },
   },
 
   {
     id:          'stargazy_pie',
     name:        'Stargazy Pie',
     description: 'Electric damage dealt is increased by 2%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 2 },
+    buff:        { electDmgPct: 2 },
   },
 
   {
     id:          'sweet_sour_lion',
     name:        'Sweet and Sour Lion\'s Head',
     description: 'Corrosion damage dealt is increased by 2%. Considered a buff, cannot be cleansed.',
-    buff:        { dmgPct: 2 },
+    buff:        { corroDmgPct: 2 },
   },
 
   {
     id:          'tabbouleh',
     name:        'Tabbouleh',
     description: 'When dealing Corrosion damage, attack is increased by 2.5%. Considered a buff, cannot be cleansed.',
-    buff:        { atkPct: 2.5 },
+    buff:        { corroAtkPct: 2.5 },
   },
 
   {
